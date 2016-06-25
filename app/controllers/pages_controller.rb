2a578557680b89cc5e87491d1176099e2b7b68ef
Ziv Zamechek <ziv921@yahoo.com>
2016-06-25 16:15:31 -0400

    added plans to home page and added create and destroy actions to registration controller

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,9 @@ class PagesController < ApplicationController
    
 
   def home
-  
+    @plan_1 = Plan.find_by_name('Basic Membership')
+    @plan_2 = Plan.find_by_name('Silver Membership')
+    @plan_3 = Plan.find_by_name('Gold Membership')
   end
 
   def about 
--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,13 +1,23 @@
 class RegistrationsController < ApplicationController
   
-  def new
-  end
-
   def create    
-  
+    @plan = Plan.find_by_name(params[:plan_name])
+    @user = User.find(params[:user_id])
+    # attach the plan to the user
+    if @user.already_registered_for?(@plan)
+      flash[:notice] = "You are already subscribed to #{@plan.name}!"
+    else
+      flash[:notice] = "Successfully subscribed to #{@plan.name}!"
+      Registration.create(user_id: @user.id, plan_id: @plan.id)      
+    end        
+    redirect_to @plan
   end
 
   def destroy
-   
+    @plan = Plan.find_by_name(params[:plan_name])
+    @user = User.find(params[:user_id])
+    Registration.where(user_id: @user.id, plan_id: @plan.id).destroy
+    flash[:notice] = "You are  unsubscribed to #{@plan.name}!"
+    redirect_to :root
   end
 end
