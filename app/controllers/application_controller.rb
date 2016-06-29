f6d2c93a1bba9447d2c5b0648ff9003a14feb5a3
Ziv Zamechek <ziv921@yahoo.com>
2016-06-29 12:27:03 -0400

    changes layout of project and added footer and admin to user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,6 +10,7 @@ class ApplicationController < ActionController::Base
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
+
   helper_method :current_user
 
   
@@ -19,6 +20,7 @@ class ApplicationController < ActionController::Base
       redirect_to new_session_url, alert: "Please sign in first!"
     end
   end
+
   helper_method :require_signin
 
   def current_user?(user)
@@ -26,5 +28,17 @@ class ApplicationController < ActionController::Base
   end
 
   helper_method :current_user?
+
+  def current_user_admin?  
+    current_user && current_user.admin?
+  end
+
+  helper_method :current_user_admin?
+
+   def require_admin
+    unless current_user_admin?
+      redirect_to root_url, alert: "Unauthorized access!"
+    end
+  end
  
 end
