3d6a145f9aed28b733a31169437f87d5a631abec
Ziv Zamechek <ziv921@yahoo.com>
2016-06-29 11:55:25 -0400

    added user authorizations and some layouts

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,10 +5,19 @@ class ApplicationController < ActionController::Base
 
 # create an instance variable that if a session variable "user_id"  exists, 
 # will return the correspond user and assign it to the that variable.
+  private
+
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
   helper_method :current_user
 
+  
+  def require_signin
+    unless current_user
+      redirect_to new_session_url, alert: "Please sign in first!"
+    end
+  end
+  helper_method :require_signin
  
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
+  before_action :require_signin, except: [:new, :create]
+  before_action :require_correct_user, only: [:edit, :update, :destroy]
+
   def new
     @user = User.new
   end
@@ -7,9 +10,19 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       session[:user_id] = @user.id
-      redirect_to root_path
+      redirect_to root_path, notice: "Thanks for signing up!"
     else
       render :new
+    end
+  end
+
+  def edit
+
+  end
+
+  def update
+    if @user.update(user_params)
+      redirect_to @user, notice: "Account successfully updated!"
     end
   end
 
@@ -21,7 +34,20 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
+  def destroy
+    @user.destroy
+    session[:user_id] = nil
+    redirect_to root_url, alert: "Account successfully deleted!"
+  end
+
   private
+
+  def require_correct_user
+    @user = User.find(params[:id])
+    unless current_user == @user
+      redirect_to root_url
+    end
+  end
 
   def user_params
     params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
