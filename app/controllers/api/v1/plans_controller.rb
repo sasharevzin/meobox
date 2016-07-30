370bd87448bbc4f209bd27e78f2b0be3b9073a04
Ziv Zamechek <ziv921@yahoo.com>
2016-07-30 13:04:09 -0400

    ability to create and destroy plans throught api

--- app/controllers/api/v1/plans_controller.rb
+++ app/controllers/api/v1/plans_controller.rb
@@ -1,4 +1,6 @@
 class Api::V1::PlansController < ApplicationController
+  skip_before_action :verify_authenticity_token
+
   def index
     plans = Plan.all
     render(
@@ -17,5 +19,29 @@ class Api::V1::PlansController < ApplicationController
       json: plan,
       serializer: Api::V1::PlanSerializer
     )
+  end
+
+  def create
+    plan = Plan.create(plan_params)
+    render(
+      root: false,
+      status: :ok,
+      json: plan,
+      serializer: Api::V1::PlanSerializer
+    )
+  end
+
+  def destroy
+    plan = Plan.find(params[:id])
+    plan.destroy
+    render json: {
+      message: "Successfully deleted the plan"
+    }
+  end
+
+  private
+
+  def plan_params
+    params.permit(:user_id, :name, :price, :description, :active)
   end
 end
