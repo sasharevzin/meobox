839480a801c89025c35014feaf1012107574d54c
Ziv Zamechek <ziv921@yahoo.com>
2016-08-01 15:51:47 -0400

    added status code and show action to users_controller

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,6 +1,25 @@
 class Api::V1::UsersController < ApplicationController
+  # skip to make sure the request came from the correct user and it is safe
+  skip_before_filter :verify_authenticity_token
   def index
     users = User.all
-    render json: users
+    render json: users, status: :ok
+  end
+
+  def show
+    user = User.find(params[:id])
+    render json: user, status: :ok
+  end
+
+  def create
+    user = User.new user_params
+    user.save
+    render json: user
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -49,7 +49,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :plans
-      resources :users, only: [:index]
+      resources :users, only: [:index, :create]
     end
   end
 end
