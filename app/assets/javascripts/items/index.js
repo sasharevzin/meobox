5725cdc2612bc6ab553afc973ac4f6f020fdac0d
Ziv Zamechek <ziv921@yahoo.com>
2016-08-08 22:24:36 -0400

    * Moved the javascript from html to index.js file * refractoring ajax call

--- app/assets/javascripts/items/index.js
+++ app/assets/javascripts/items/index.js
@@ -1,6 +1,5 @@
-var ready;
-ready = function() {
-  $('.show_details').click(function(){
+$(document).ready(function(){
+    $('.show_details').click(function(){
     event.preventDefault();
     var item_id = $(this).data('id');
       $.ajax({
@@ -15,7 +14,38 @@ ready = function() {
         $("#url").html(data['url']);
       });
   });
-};
 
-$(document).ready(ready);
-$(document).on('page:load', ready);
+    function row_html(data) {
+      var my_html = "" 
+      
+      my_html += '<div class="col-md-3">';
+      my_html += '  <div class="thumbnail">';
+      my_html += '    <img src=""></img>';
+      my_html += '    <div class="caption">';
+      my_html += '      <h3>' + data.title + '</h3>';
+      my_html += '      <p>' + data.description + '</p>';
+      my_html += '      <p>' + data.formatted_price + '</p>';
+      my_html += '      <p>' + data.size + 'lbs</p>';
+      my_html += '      <p>' + data.url + '</p>';
+      my_html += '    </div>';
+      my_html += '  </div>';
+      my_html += '</div>';
+
+      return my_html;
+    }
+
+    $('#search_button').click(function(){
+      event.preventDefault();
+      var search = $('#ajax_search').val();
+    $.ajax({
+      url:  '/api/v1/items?search=' + search,
+      dataType: 'json'
+    }).done(function(data) {
+      var data_to_html = "";
+      for (var i = 0; i < data.length; i++) {
+        data_to_html += row_html(data[i]);
+      }
+      $('#data_list').html(data_to_html);
+    });
+  });
+});
